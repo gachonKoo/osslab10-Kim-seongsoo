41d7514319fed68d1bb535fa6a6671631c082535
Kim-seongsoo <rlatjdtn9643@naver.com>
2025-11-09 16:33:40 +0900

    divisors.py

--- divisors.py
+++ divisors.py
@@ -1,9 +1,5 @@
 def get_divisors(n):
-    divisors = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            divisors.append(i)
-    return divisors
+    return [i for i in range(1, n + 1) if n % i == 0]
 
 
 if __name__ == "__main__":

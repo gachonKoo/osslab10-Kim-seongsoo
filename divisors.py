9868f31b8fb25b5c8022d042653b1b50dafe7107
Kim-seongsoo <rlatjdtn9643@naver.com>
2025-11-09 16:41:31 +0900

    divisors.py

--- divisors.py
+++ divisors.py
@@ -1,11 +1,12 @@
+import sys
+
 def get_divisors(n):
-    result = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            result.append(i)
-    return result
+    """Return a list of all positive divisors of n."""
+    return [i for i in range(1, n + 1) if n % i == 0]
 
 
 if __name__ == "__main__":
-    print(get_divisors(12))
-
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+        result = get_divisors(n)
+        print(result)
